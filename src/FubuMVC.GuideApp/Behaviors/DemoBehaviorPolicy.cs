2d632900dccee713cc3d44057784380ac18fb40d
Chad Myers <chad.myers@gmail.com>
2010-02-13 00:08:52 -0600

    Added Advanced Behaviors guide and start of basic behaviors guide

---
+++ src/FubuMVC.GuideApp/Behaviors/DemoBehaviorPolicy.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using FubuMVC.Core;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
+
+namespace FubuMVC.GuideApp.Behaviors
+{
+    public class DemoBehaviorPolicy : IConfigurationAction
+    {
+        public void Configure(BehaviorGraph graph)
+        {
+            graph.Behaviors
+                .Where(c => c.FirstCall().Method.Name == "Home")
+                .Each(c => c.Prepend(new Wrapper(typeof (DemoBehaviorForSelectActions))));
+        }
+    }
+}
--- src/FubuMVC.GuideApp/Global.asax.cs
+++ src/FubuMVC.GuideApp/Global.asax.cs
@@ -1,8 +1,19 @@
-﻿using FubuMVC.StructureMap.Bootstrap;
+﻿using FubuMVC.Core;
+using FubuMVC.StructureMap.Bootstrap;
 
 namespace FubuMVC.GuideApp
-{
+{ /* This is the Global used for the Getting_started guide. DO NOT DELETE
     public class Global : FubuStructureMapApplication
     {
+    }
+  */
+   
+    public class Global : FubuStructureMapApplication
+    {
+        public override FubuRegistry GetMyRegistry()
+        {
+            return new AdvBehaviorGuideRegistry();
+        }
+        
     }
 }
