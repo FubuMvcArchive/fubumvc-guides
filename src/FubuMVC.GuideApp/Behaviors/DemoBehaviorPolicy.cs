8d3d551c2b735becf80d7bf4c19cbf391f04c594
Chad Myers <chad.myers@gmail.com>
2010-05-28 19:43:35 -0500

    Upgraded FubuMVC references and fixed compile errors. Added guide for FubuRegistry configuring actions

--- src/FubuMVC.GuideApp/Behaviors/DemoBehaviorPolicy.cs
+++ src/FubuMVC.GuideApp/Behaviors/DemoBehaviorPolicy.cs
@@ -1,6 +1,5 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
-using FubuMVC.Core;
 using FubuMVC.Core.Registration;
 using FubuMVC.Core.Registration.Nodes;
 
---
+++ src/FubuMVC.GuideApp/Examples/configuring_actions_RegistryExamples.cs
@@ -0,0 +1,24 @@
+using System;
+using FubuMVC.Core;
+
+namespace FubuMVC.GuideApp.Examples
+{
+    public class configuring_actions_RegistryExamples : FubuRegistry
+    {
+        public configuring_actions_RegistryExamples()
+        {
+            Actions
+                .IncludeTypesNamed(x => x.EndsWith("Controller"))
+                .IncludeTypesImplementing<IAction>();
+
+            Routes
+                .IgnoreControllerNamespaceEntirely()
+                .IgnoreMethodsNamed("Index");
+
+            Routes.ConstrainToHttpMethod(x => x.Method.Name == "Post", "POST");
+
+        }
+    }
+
+    public interface IAction{}
+}
