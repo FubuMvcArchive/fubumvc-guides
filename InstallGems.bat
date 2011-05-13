dba3c27b75e30952b4ea41a9ac2b79edb9b88e63
Chad Myers <chad.myers@gmail.com>
2011-05-13 11:12:42 -0500

    Added RedCloth dependency

--- InstallGems.bat
+++ InstallGems.bat
@@ -3,3 +3,6 @@
 
 @ECHO *** Installing Albacore (build support tools)
 @call gem install albacore --no-rdoc --no-ri
+
+@ECHO *** Installing RedCloth (Textfile generator)
+@call gem install -v=4.2.2 --platform=mswin32 RedCloth --no-rdoc --no-ri
